f444896e5afbf3b6ceabafeeb3f1a13837f0bd8d
Lee <104661388+leeghimire@users.noreply.github.com>
2024-06-18 21:46:21 -0400

    terrible test

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -2,20 +2,32 @@
   imports = [
     ./hardware-configuration.nix
     ./packages.nix
+  ];
 
-    ./modules/bootloader.nix
+  disabledModules = [
     ./modules/sound.nix
     ./modules/zram.nix
     ./modules/env.nix
-    ./modules/user.nix
     ./modules/xserver.nix
-    ./modules/nm.nix
     ./modules/virtmanager.nix
     ./modules/trim.nix
   ];
 
-  disabledModules = [
-  ];
+  users = {
+    #defaultUserShell = pkgs.zsh;
+
+    users.lee = {
+      isNormalUser = true;
+      description = "Lee NLN";
+      extraGroups = [ "networkmanager" "wheel" "input" "libvirtd" ];
+      packages = with pkgs; [];
+    };
+  };
+
+  # Enable automatic login for the user.
+  services.getty.autologinUser = "lee";
+
+  networking.networkmanager.enable = true;
 
   nixpkgs.overlays = [ inputs.polymc.overlay ];
 
@@ -24,6 +36,18 @@
   time.timeZone = "America/Toronto"; # Set your time zone.
 
   i18n.defaultLocale = "en_US.UTF-8"; # Select internationalisation properties.
+
+  boot.initrd.luks.devices = {
+    crypted = {
+      device = "/dev/disk/by-uuid/<UUID>"; #change
+      preLVM = true;
+    };
+  };
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.initrd.kernelModules = [];
+  boot.kernelParams = [ "psmouse.synaptics_intertouch=0" ]; 
 
   system.stateVersion = "23.05"; # Don't change it bro
 }
