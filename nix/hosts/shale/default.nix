000eec9db26029e51db1a31ac8b8996eea70ba8d
Lee Ghimire <hello@leeghimire.com>
2026-03-07 22:18:08 -0500

    grub

--- nix/hosts/shale/default.nix
+++ nix/hosts/shale/default.nix
@@ -8,8 +8,9 @@
   networking.hostName = "shale";
   networking.enableIPv6 = false;
 
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "/dev/nvme0n1";
+  boot.loader.grub.useOSProber = true;
 
   nixpkgs.config.allowUnfree = true;
 
