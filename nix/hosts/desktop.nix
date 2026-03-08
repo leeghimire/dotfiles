8aff80b8f0eb1ca72e0d7e158e166ab1c8b0351e
Lee Ghimire <hello@leeghimire.com>
2026-03-07 20:52:44 -0500

    setting up the desktop

--- nix/hosts/desktop.nix
+++ nix/hosts/desktop.nix
@@ -1,8 +1,73 @@
-{ lib, options, ... }:
-let
-  has = lib.hasAttrByPath;
-in {
-  config = lib.mkIf (has [ "networking" "hostName" ] options) {
-    networking.hostName = "desktop";
+{ config, pkgs, ... }: {
+  networking.hostName = "shale";
+
+  nixpkgs.config.allowUnfree = true;
+
+  networking.networkmanager.enable = true;
+  services.tailscale.enable = true;
+
+  programs.hyprland = {
+    enable = true;
+    withUWSM = true;
+    xwayland.enable = true;
   };
+
+  services.displayManager.sddm = {
+    enable = true;
+    wayland.enable = true;
+  };
+  services.displayManager.defaultSession = "hyprland";
+
+  services.xserver.enable = true;
+  services.xserver.videoDrivers = [ "nvidia" ];
+
+  hardware.graphics.enable = true;
+  hardware.nvidia = {
+    modesetting.enable = true;
+    nvidiaSettings = true;
+    open = false;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
+
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+  };
+  services.blueman.enable = true;
+
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true;
+    dedicatedServer.openFirewall = true;
+  };
+
+  programs.firefox = {
+    enable = true;
+    nativeMessagingHosts.packages = [ pkgs.tridactyl-native ];
+    policies.ExtensionSettings = {
+      "tridactyl.vim@cmcaine.co.uk" = {
+        installation_mode = "force_installed";
+        install_url = "https://addons.mozilla.org/firefox/downloads/latest/tridactyl-vim/latest.xpi";
+      };
+    };
+  };
+
+  environment.sessionVariables = {
+    NIXOS_OZONE_WL = "1";
+    TERMINAL = "ghostty";
+  };
+
+  environment.systemPackages = with pkgs; [
+    cudatoolkit
+    discord
+    ghostty
+  ];
 }
